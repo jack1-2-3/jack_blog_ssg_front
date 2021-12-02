b8a5beb3e108d945f0be48983afb2ac2240b4391
jack <jack.programming123@gmail.com>
2021-12-02 09:11:57 +0900

    記事コンテンツを表示

--- components/presentationals/shared/Post.vue
+++ components/presentationals/shared/Post.vue
@@ -3,14 +3,17 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          <p>{{ postId ? posts[postId].title : posts[0].title }}</p>
+          <p>{{ post.title }}</p>
         </v-card-title>
         <v-card-text>
-          {{ postId ? posts[postId].content : posts[0].content }}
+          <!-- eslint-disable-next-line vue/no-v-html -->
+          <div v-html="content"></div>
           <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+            <em
+              ><small>&mdash; {{ post.author }}</small></em
+            >
           </div>
-          <hr class="my-3">
+          <hr class="my-3" />
           <a
             href="https://nuxtjs.org/"
             target="_blank"
@@ -18,7 +21,7 @@
           >
             Nuxt Documentation
           </a>
-          <br>
+          <br />
           <a
             href="https://github.com/nuxt/nuxt.js"
             target="_blank"
@@ -29,13 +32,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
+          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
@@ -44,17 +41,29 @@
 
 <script lang="ts">
 import Vue from 'vue'
+
 export default Vue.extend({
   props: {
     posts: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
   data() {
     return {
-      postId: (this as any).$route.params.id
+      postId: (this as any).$route.params.id,
     }
-  }
+  },
+  computed: {
+    post() {
+      if(!(this as any).postId) {
+        return (this as any).posts[0]
+      }
+      return (this as any).posts[(this as any).postId]
+    },
+    content() {
+      return (this as any).$md.render((this as any).post.content)
+    },
+  },
 })
 </script>
