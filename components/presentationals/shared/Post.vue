d8813dfc897d4334536c8a8ef9cbf4e9134e2b7a
jack <jack.programming123@gmail.com>
2021-12-05 08:22:20 +0900

    記事装飾を追加

--- components/presentationals/shared/Post.vue
+++ components/presentationals/shared/Post.vue
@@ -3,21 +3,26 @@
     <v-col cols="12" sm="8" md="6">
       <v-card>
         <v-card-title class="headline">
-          <p>{{ post.title }}</p>
+          <h1 class="title">{{ post.title }}</h1>
         </v-card-title>
         <v-card-text>
           <!-- eslint-disable-next-line vue/no-v-html -->
           <div class="content" v-html="content"></div>
           <div class="text-xs-right">
             <em
-              ><small>&mdash; {{ post.author }}</small></em
+              ><small>&mdash; ジャック(鶴田翔大)</small></em
             >
           </div>
           <hr class="my-3" />
+          <h3 class="writerTitle">この記事を書いた人</h3>
+          <p>ジャック(鶴田翔大) &mdash; エンジニア</p>
+          <p>元ニート。暇だったからプログラミング始めたら、面白すぎて仕事にしちゃいました。</p>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+          <v-btn class="white--text" color="blue" href="https://twitter.com/jack_ASDprogram" target="_blank"> Twitter </v-btn>
+          <v-btn class="white--text" color="green" href="https://qiita.com/jack1-2-3" target="_blank"> Qiita </v-btn>
+          <v-btn class="white--text" color="black" href="https://github.com/jack1-2-3" target="_blank"> Github </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
@@ -60,9 +65,41 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+.title {
+  text-decoration: underline greenyellow 5px;
+}
 .content >>> img {
   width: 80%;
   display: block;
   margin: auto;
+}
+.content >>> h1 {
+  margin-bottom: 5px;
+  padding-left: 10px;
+  border-left: grey 5px solid;
+}
+.content >>> h2 {
+  margin-bottom: 5px;
+  padding-left: 10px;
+  border-left: grey 5px solid;
+}
+.content >>> h3 {
+  margin-bottom: 5px;
+  padding-left: 10px;
+  border-left: grey 5px solid;
+}
+.content >>> h4 {
+  margin-bottom: 5px;
+  padding-left: 10px;
+  border-left: grey 5px solid;
+}
+.content >>> h5 {
+  margin-bottom: 5px;
+  padding-left: 10px;
+  border-left: grey 5px solid;
+}
+.writerTitle {
+  margin-bottom: 3px;
+  text-decoration: underline yellowgreen 3px;
 }
 </style>
